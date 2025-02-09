3714dfdf7fad7e35bde663c82f41d3c4d760f88a
Ameen <tkameen855@gmail.com>
2025-02-09 11:06:01 -0500

    Integrated Redux for cart functionality in ProductList

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,6 +4,17 @@ import CartItem from './CartItem';
 function ProductList() {
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+          ...prevState,
+          [product.name]: true, // Track added items
+        }));
+      };
+      
+
 
     const plantsArray = [
         {
@@ -268,6 +279,27 @@ const handlePlantsClick = (e) => {
         </div>
         {!showCart? (
         <div className="product-grid">
+            {plantsArray.map((category, index) => (
+  <div key={index}>
+    <h1><div>{category.category}</div></h1>
+    <div className="product-list">
+      {category.plants.map((plant, plantIndex) => (
+        <div className="product-card" key={plantIndex}>
+          <img className="product-image" src={plant.image} alt={plant.name} />
+          <div className="product-title">{plant.name}</div>
+          <div className="product-description">{plant.description}</div>
+          <div className="product-cost">{plant.cost}</div>
+          <button  
+            className="product-button" 
+            onClick={() => handleAddToCart(plant)}
+          >
+            {addedToCart[plant.name] ? "Added" : "Add to Cart"}
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+))}
 
 
         </div>
